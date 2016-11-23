46bb1fe325196e3e2a4e683d20a60dc816a7f3e0
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2016-11-23 12:16:52 -0200

    Add comments

--- src/hello.pl
+++ src/hello.pl
@@ -3,3 +3,12 @@
 # This will print "Hello, World"
 print "Hello, world\n";
 print("Hello, world\n");
+
+# This is a comment in perl
+
+=begin comment
+This is all part of multiline comment.
+You can use as many lines as you like
+These comments will be ignored by the
+compiler until the next =cut is encountered.
+=cut
