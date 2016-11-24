0616cab7eb0eb36a31289b2200d87da858722caa
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2016-11-24 12:10:27 -0200

    Scalar operations

---
+++ src/scalar-operations.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+$str = "hello" . "world";       # Concatenates strings.
+$num = 5 + 10;                  # adds two numbers.
+$mul = 4 * 5;                   # multiplies two numbers.
+$mix = $str . $num;             # concatenates string and number.
+
+print "str = $str\n";
+print "num = $num\n";
+print "mix = $mix\n";
